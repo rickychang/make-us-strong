7eb72009fe3ff41f0b02e5966d38177434119dfb
Ricky Chang <ricchang@ebay.com>
2012-10-25 00:56:28 -0400

    fixing sounds

--- makeUsStrong.js
+++ makeUsStrong.js
@@ -11,12 +11,27 @@ function extractStrengthener(hostname) {
     return strengthener;
 };
 
+var trekSounds = [
+'http://www.trekcore.com/audio/computer/sequences/astrometrics_controls.mp3',
+'http://www.trekcore.com/audio/computer/energize.mp3',
+'http://www.trekcore.com/audio/computer/hailbeep_clean.mp3', 
+'http://www.trekcore.com/audio/computer/computerbeep_69.mp3', 
+'http://www.trekcore.com/audio/computer/computerbeep_37.mp3',
+'http://www.trekcore.com/audio/computer/computer_work_beep.mp3',
+'http://www.trekcore.com/audio/computer/critical.mp3',
+'http://www.trekcore.com/audio/computer/processing2.mp3',
+'http://www.trekcore.com/audio/computer/scrscroll2.mp3',
+'http://www.trekcore.com/audio/computer/sequences/tactical_beep_sequence.mp3'
+];
+
  
 http.createServer(function (httpRequest, httpResponse) {
     var whatMakesUsStrong = extractStrengthener(httpRequest.headers.host)
     if (whatMakesUsStrong) {
         console.log(whatMakesUsStrong);
         var memeGeneratorApiURL = 'http://version1.api.memegenerator.net/Instance_Create?username=pakledSOS&password=engage&languageCode=en&generatorID=1568864&imageID=6447360&text0=' + encodeURI(whatMakesUsStrong) + '&text1=makes%20us%20strong';
+        console.log(memeGeneratorApiURL);
+        var randSound = trekSounds[Math.floor(Math.random() * trekSounds.length)]
         request(memeGeneratorApiURL, function (error, memeResponse, body) {
             if (!error && memeResponse.statusCode == 200) {
                     var memeImageUrl = JSON.parse(body)['result']['instanceImageUrl']
@@ -26,6 +41,7 @@ http.createServer(function (httpRequest, httpResponse) {
                     httpResponse.write('<title>' + whatMakesUsStrong + ' MAKES US STRONG</title>');
                     httpResponse.write('</head><body bgcolor="#000000">');
                     httpResponse.write('<img src="' + memeImageUrl + '"/>');
+                    httpResponse.write('<audio src="' + randSound + '" autoplay="true" autobuffer></audio>');
                     httpResponse.write('</body></html>');
                     httpResponse.end();
             }
